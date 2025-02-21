681fc26a3146996fd1860a2d389d3127ed5d8462
Keya <keyamondal549@gmail.com>
2025-02-21 22:34:35 +0600

    created api to get & post tasks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require('express')
-const cors=require('cors')
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-const app=express()
-const port=process.env.PORT||5000
+const app = express()
+const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json())
@@ -22,25 +22,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    const userCollection=client.db('TaskManagement').collection('users')
+    const dbs = client.db('TaskManagement')
+    const userCollection = dbs.collection('users')
+    const taskCollection = dbs.collection('tasks')
 
-
-    app.post('/users',async(req,res)=>{
-      const newUser=req.body
-      const result=await userCollection.insertOne(newUser)
+    // For creating users
+    app.post('/users', async (req, res) => {
+      const newUser = req.body
+      const result = await userCollection.insertOne(newUser)
       res.send(result)
     })
 
-    app.get('/users',async(req,res)=>{
-      const cursor=userCollection.find()
-      const result=await cursor.toArray()
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // For adding Tasks
+    app.post('/tasks', async (req, res) => {
+      const item = req.body
+      const { email } = item
+      const user = await userCollection.findOne({ email })
+      const result = await taskCollection.insertOne(item)
+      res.send(result)
+
+    })
+
+    app.get('/tasks', async (req, res) => {
+      const cursor = taskCollection.find()
+      const result = await cursor.toArray()
       res.send(result)
     })
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -48,10 +66,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('server running')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`server running on port : ${port}`)
 })
